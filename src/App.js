e64b31538575f2006302f7a0bd3140373870d02f
gavinev98 <gavin.everett48@hotmail.com>
2020-12-14 10:55:08 +0000

    applied header to appjs file.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-
+    <header>
+      <h1>Gavins Daily List</h1>
+    </header>
     </div>
   );
+
+
 }
 
 export default App;
