e611fdd39e93f3bbd505ec3f87b864b5e4057719
gavinev98 <gavin.everett48@hotmail.com>
2020-12-15 16:05:52 +0000

    passing toDo list data to the ToDoList.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   <h1>Gavins Daily List </h1>
     </header>
     <FormFilter toDo={toDo} setToDoList={setToDoList} inputText={inputText} setInputText={setInputText} />
-    <ToDoList   />
+    <ToDoList toDo={toDo}  />
     </div>
   );
 
