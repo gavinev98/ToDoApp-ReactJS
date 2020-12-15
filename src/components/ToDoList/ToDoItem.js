d1f2b029c41bbfa44736b7b3c76b27b018da741b
gavinev98 <gavin.everett48@hotmail.com>
2020-12-15 16:03:32 +0000

    updating styling of buttons.

--- src/components/ToDoList/ToDoItem.js
+++ src/components/ToDoList/ToDoItem.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+
+
+const ToDoItem = () => {
+
+    return (
+        <div className="todo">
+            <li className='todo-item'>HEYYYYYYYY</li>
+            <button className="complete-btn" ><i className="fas fa-check"></i></button>
+            <button className="trash-btn"><i className="fas fa-trash"></i></button>
+        </div>
+
+    );
+
+
+}
+
+export default ToDoItem;
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+//importing ToDoItems for rendering.
+import ToDoItem from '../ToDoList/ToDoItem';
+
 const ToDoList = () => {
 
     return (
         <div className="todo-container">
-        <ul className="todo-list"></ul>
+        <ul className="todo-list">
+            <ToDoItem />
+            </ul>
       </div>
     );
 
