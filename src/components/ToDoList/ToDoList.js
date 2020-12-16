8d702305649db7064d219092e2bf8c979e0ff4c4
gavinev98 <gavin.everett48@hotmail.com>
2020-12-16 14:41:50 +0000

    passing todo list down to todoitem component.

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -3,13 +3,13 @@ import React from 'react';
 //importing ToDoItems for rendering.
 import ToDoItem from '../ToDoList/ToDoItem';
 
-const ToDoList = ({toDo}) => {
+const ToDoList = ({toDo, setToDoList}) => {
     
     return (
         <div className="todo-container">
         <ul className="todo-list">
         {toDo.map(todoItems => (
-           <ToDoItem key={todoItems.id} id={todoItems.id} completed={todoItems.completed} text={todoItems.text} />
+           <ToDoItem toDo={toDo} setToDoList={setToDoList} key={todoItems.id} id={todoItems.id} completed={todoItems.completed} text={todoItems.text}  />
         ))}
             </ul>
       </div>
