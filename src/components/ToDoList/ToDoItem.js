712720572b8f7f2679dc1096af2042ada6ec3ace
gavinev98 <gavin.everett48@hotmail.com>
2020-12-16 15:02:48 +0000

    implemented delete handler to toDo list.

--- src/components/ToDoList/ToDoItem.js
+++ src/components/ToDoList/ToDoItem.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 
 
-const ToDoItem = ({id , completed, text}) => {
+const ToDoItem = ({id , completed, text, toDo, setToDoList}) => {
+
+    //events for deleting / ticking off item from list
+
+    const deleteHandler = (id) => {
+        //deleting item clicked using the filter function.
+        setToDoList(toDo.filter(todo => todo.id !== id));
+
+    }
 
     return (
         <div className="todo">
             <li className='todo-item'>{text}</li>
             <button className="complete-btn" ><i className="fas fa-check"></i></button>
-            <button className="trash-btn"><i className="fas fa-trash"></i></button>
+            <button onClick={() => deleteHandler(id)} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
 
     );
